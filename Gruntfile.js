ff889d7f6c9a3e26272fe0e86edbba978feb3a66
Choi Goon-ho <caiguanhao@gmail.com>
2014-02-07 23:27:01 +0800

    add package.json; add index.html and others;

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    connect: {
+      server: {
+        options: {
+          port: 3000,
+          base: [ 'assets', 'public' ]
+        }
+      }
+    },
+    watch: {
+      options: {
+        livereload: true
+      },
+      index: {
+        files: [ 'public/index.html' ]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', [ 'connect', 'watch' ]);
+
+};
