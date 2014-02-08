f978a9b79693242ee9a0e0ec6ac54e0f44f417d5
Choi Goon-ho <caiguanhao@gmail.com>
2014-02-08 17:29:51 +0800

    add a help document and grunt task to generate index;

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,4 +27,31 @@ module.exports = function(grunt) {
 
   grunt.registerTask('default', [ 'connect', 'watch' ]);
 
+  grunt.registerTask('make_help_index', 'Generate help index JSON file', function() {
+    var path = require('path');
+    var help_dir = 'posts/help';
+    var help_files = grunt.file.expand({
+      cwd: help_dir
+    }, '*.md');
+    var index = [];
+    help_files.forEach(function(help_file) {
+      var file = grunt.file.read(path.join(help_dir, help_file));
+      var yfm = file.match(/^---((.|\n)*)---\n/);
+      var title = '';
+      if (yfm) {
+        yfm = yfm[1].trim();
+        title = yfm.match(/^title:\s*(.*)$/mi)
+        if (title) title = title[1].trim();
+        title = title.replace(/^(["']?)(.*)\1$/, '$2');
+      }
+      var slug = path.basename(help_file, path.extname(help_file));
+      index.push({
+        slug: slug,
+        title: title
+      });
+    });
+    grunt.file.write(path.join('public', 'help.json'),
+      JSON.stringify(index, null, 2) + '\n');
+  });
+
 };
