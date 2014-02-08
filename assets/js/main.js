61c68efe4ba142e3a0dd3f6c69e677c5edfd31f0
Choi Goon-ho <caiguanhao@gmail.com>
2014-02-08 14:42:44 +0800

    change crypto method;

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,35 @@ App.factory('Builds', function($http) {
 });
 
 function BuildsController($scope, Builds) {
+  $scope.cryptos = [
+    {
+      key: 'md5sum',
+      name: 'MD5',
+      active: true
+    },
+    {
+      key: 'shasum',
+      name: 'SHA-1',
+    }
+  ];
+  $scope.currentCrypto = function() {
+    for (var i = 0; i < $scope.cryptos.length; i++) {
+      if ($scope.cryptos[i].active) return $scope.cryptos[i];
+    }
+    return $scope.cryptos[0];
+  };
   Builds.success(function(builds) {
     $scope.builds = builds;
   });
 }
+
+App.directive('crypto', function() {
+  return function(scope, element, attrs) {
+    element.bind('click', function() {
+      scope.cryptos.forEach(function(crypto) {
+        crypto.active = (crypto.key === attrs.crypto);
+      });
+      scope.$apply();
+    });
+  };
+});
